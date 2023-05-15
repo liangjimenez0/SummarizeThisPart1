734008394f909a0aa88037195f74aff878e07ec8
Liang jimenez <liangjimenez@yahoo.com>
2023-05-15 14:48:44 -0400

    Sorts the array list of files by the date it was modified. Fixes #6.

---
+++ src/main/java/cs3500/pa01/FileSortByModified.java
@@ -0,0 +1,12 @@
+package cs3500.pa01;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
+public class FileSortByModified implements Comparator<FileInformation>{
+  @Override
+  public int compare(FileInformation o1, FileInformation o2) {
+    return o2.getDateModified().compareTo(o1.getDateModified());
+  }
+}
