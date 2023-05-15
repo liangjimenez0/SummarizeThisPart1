28d3f77074f43e9b8eaa8b5502606b1e1e1b5e6e
Liang jimenez <liangjimenez@yahoo.com>
2023-05-15 14:46:32 -0400

    Compiles the list of files into one string. Fixes #7.

---
+++ src/main/java/cs3500/pa01/FileCompiler.java
@@ -0,0 +1,35 @@
+package cs3500.pa01;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class FileCompiler {
+  StringBuilder builder = new StringBuilder();
+  public void compile(ArrayList<FileInformation> mdFiles) throws IOException {
+
+    ArrayList<Path> pathList = new ArrayList<>();
+    Scanner input = null;
+
+    for (FileInformation p : mdFiles) {
+      pathList.add(p.getFilePath());
+    }
+
+    for (Path p : pathList) {
+      try {
+        input = new Scanner(p);
+      } catch (IOException e) {
+        System.exit(1);
+      }
+
+      while (input.hasNext()) {
+        builder.append(input.nextLine()).append(System.lineSeparator());
+      }
+    }
+  }
+  public String getCompiled() {
+    return builder.toString();
+  }
+}
