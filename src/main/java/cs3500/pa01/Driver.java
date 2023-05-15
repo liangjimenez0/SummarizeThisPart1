893f6ce2d0a7eff130f7513e9a4db55e406d38f2
Liang jimenez <liangjimenez@yahoo.com>
2023-05-15 14:45:40 -0400

    Deals with arguments from main class and delegates to each of the classes. Fixes #8.

--- src/main/java/cs3500/pa01/Driver.java
+++ src/main/java/cs3500/pa01/Driver.java
@@ -1,15 +1,33 @@
 package cs3500.pa01;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Collections;
+import java.util.Scanner;
+
 /**
  * This is the main driver of this project.
  */
 public class Driver {
-  /**
-   * Project entry point
-   *
-   * @param args - no command line args required
-   */
-  public static void main(String[] args) {
-    System.out.println("Hello from PA01 Template Repo");
+
+  public static void driver(Path path, OrderFlag orderingFlag, Path outputPath) throws IOException {
+
+    FileTreeWalkerVisitor reader = new FileTreeWalkerVisitor();
+    Files.walkFileTree(path, reader);
+
+    if (orderingFlag.equals(OrderFlag.FILENAME)) {
+      reader.getMdFiles().sort(new FileSortByName());
+    } else if (orderingFlag.equals(OrderFlag.CREATED)) {
+      reader.getMdFiles().sort(new FileSortByCreated());
+    } else {
+      reader.getMdFiles().sort(new FileSortByModified());
+    }
+
+    FileCompiler compiler = new FileCompiler();
+    compiler.compile(reader.getMdFiles());
+
+    FileProcessor processor = new FileProcessor();
+    processor.processFiles(compiler.getCompiled(), outputPath);
   }
 }
