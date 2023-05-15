9ae34af1710ba7f05d62734c48ca87699609593b
Liang jimenez <liangjimenez@yahoo.com>
2023-05-15 10:22:02 -0400

    Added main class that deals with users' arguments. Fixes #10.

---
+++ src/main/java/cs3500/pa01/Main.java
@@ -0,0 +1,39 @@
+package cs3500.pa01;
+
+import java.io.IOException;
+import java.nio.file.InvalidPathException;
+import java.nio.file.Path;
+
+public class Main {
+  Path path;
+  OrderFlag orderingFlag;
+  Path outputPath;
+
+  public static void main(String[] args) throws IOException {
+
+    Path path = null;
+
+    try {
+      path = Path.of(args[0]);
+    } catch (InvalidPathException e) {
+      System.err.println("Cannot assign path");
+    }
+
+    OrderFlag orderingFlag = null;
+    Path outputPath = null;
+
+    switch (args[1]) {
+      case "FILENAME" -> orderingFlag = OrderFlag.FILENAME;
+      case "CREATED" -> orderingFlag = OrderFlag.CREATED;
+      case "MODIFIED" -> orderingFlag = OrderFlag.MODIFIED;
+    }
+
+    try {
+      outputPath = Path.of(args[2]);
+    } catch (InvalidPathException e) {
+      System.err.println("Cannot assign path");
+    }
+
+    Driver.driver(path, orderingFlag, outputPath);
+  }
+}
